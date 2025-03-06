74f5cc38633e5e668c7477db8173f24733bd9a06
nikhildubey-23 <ravannick0089@gmail.com>
2025-03-06 22:08:38 +0530

    creating a lexer function and creating a loop which is equal to the length of the cursor and also add the condition to remove the whitespaces

--- compiler.js
+++ compiler.js
@@ -1,4 +1,22 @@
+function lexer(input){
+    const tokens = [];
+    let cursor = 0; // cursor to keep track of the current position in the input string
+    while(cursor < input.length){
+        let char = input[cursor];
 
+        //skip whitespace
+        if (/\s/.test(char)){
+            cursor++;
+            continue;
+        }
+    }
+}
 function compiler(input){
     const token = lexer(input);
 }
+const code = `
+dg x = 10
+dg y = 20
+dg z = x + y
+dgpr z
+`
